7298dd842f7c4e549fd520ebf036a27d6711443b
Tina Calleja <124672039+usertina@users.noreply.github.com>
2026-03-17 14:55:11 +0100

    Update script.jsañadido temporadas

--- script.js
+++ script.js
@@ -3,9 +3,8 @@
 // ================================================================
 const EPISODIOS = [
     {
-        titulo: "Shema",
+        titulo: "Te he llamado por tu nombre",
         descripcion: "Temporada 1, Episodio 1",
-        // 🔴 SOLO ESTE ENLACE HA SIDO CAMBIADO al formato .m3u8
         url: "https://cloud-s84178-118-164.spcdn.cc/hls/h75azyppfle2alhulmzt4lm5dsucxda2f7hzw3lbwumg3a5tb5i2lckyj2ra/master.m3u8"
     },
     {
@@ -14,73 +13,193 @@ const EPISODIOS = [
         url: "https://box-1102-t.vmeas.cloud/hls/,xqx2pf4unzokjiqbtgssjiyd5gvkx4rftjckquapfued363nsjwxs36p2iya,.urlset/master.m3u8"
     },
     {
-        titulo: "Jesus Loves Me",
+        titulo: "Jesús ama a los más pequeños",
         descripcion: "Temporada 1, Episodio 3",
         url: "https://cloud-s84178-118-164.spcdn.cc/hls/h75azbfnfle2alhulm7d4ogejlqnqzbt4uh5dyelthlfvechtqaqlpfjob4q/index-v1-a1.m3u8"
     },
     {
-        titulo: "The Rock on Which It Is Built",
+        titulo: "Sobre esta roca edificaré",
         descripcion: "Temporada 1, Episodio 4",
         url:"https://cloud-s84178-118-164.spcdn.cc/hls/h75a5z7pfle2alhulmzt47e5cctsyu3rglhmftgmbrfr4kcjviapuciqk4sq/index-v1-a1.m3u8"
     },
     {
-        titulo: "The Wedding Gift",
+        titulo: "El regalo de boda",
         descripcion: "Temporada 1, Episodio 5",
         url: "https://cloud-s84178-118-164.spcdn.cc/hls/h75a5zhpfle2alhulmzt4posaslpf3p4heoe4a6hayb2gomw7ohbo6si7z4a/index-v1-a1.m3u8"
     },
     {
-        titulo: "Indescribable Virtue",
+        titulo: "Compasión infinita",
         descripcion: "Temporada 1, Episodio 6",
         url: "https://cloud-s84178-118-164.spcdn.cc/hls/h75az2xpfle2alhulmzt4pmud4jvojpvyd5bhsfka5cceujs2e2v4455wtca/index-v1-a1.m3u8"
     },
     {
-        titulo: "Invitations",
+        titulo: "Invitaciones",
         descripcion: "Temporada 1, Episodio 7",
         url: "https://cloud-s84178-118-164.spcdn.cc/hls/h75a3zppfle2alhulmzt4jwmdcc47bilgzy434mderxcjzhphawbqqtxmjxa/master.m3u8"
     },
     {
-        titulo: "I Am He",
+        titulo: "Yo soy el que soy",
         descripcion: "Temporada 1, Episodio 8",
         url: "https://stream-4891-212-105.pkcdn.org/juohpheqgyikkfn2mcw7qoanm7dn3ybrd3y75ulwbcrjfjmnmxikxnckta/v.mp4"
     },
     {
-        titulo: "Episodio 9",
+        titulo: "Trueno",
         descripcion: "Temporada 2, Episodio 1",
         url: "https://watch.thechosen.tv/video/184683594353?language=en&position=0"
     },
     {
-        titulo: "Episodio 10",
+        titulo: "Te ví",
         descripcion: "Temporada 2, Episodio 2",
         url: "https://watch.thechosen.tv/video/184683594354?language=en&position=0"
     },
     {
-        titulo: "Episodio 11",
+        titulo: "Mateo 4:24",
         descripcion: "Temporada 2, Episodio 3",
         url: "https://watch.thechosen.tv/video/184683594355?language=en&position=0"
     },
     {
-        titulo: "Episodio 12",
+        titulo: "La ocasión perfecta",
         descripcion: "Temporada 2, Episodio 4",
         url: "https://watch.thechosen.tv/video/184683594356?language=en&position=0"
     },
     {
-        titulo: "Episodio 13",
+        titulo: "Espíritu",
         descripcion: "Temporada 2, Episodio 5",
         url: "https://watch.thechosen.tv/video/184683594357?language=en&position=0"
     },
     {
-        titulo: "Episodio 14",
+        titulo: "Ilícito",
         descripcion: "Temporada 2, Episodio 6",
         url: "https://watch.thechosen.tv/video/184683594358?language=en&position=0"
     },
     {
-        titulo: "Episodio 15",
+        titulo: "Ajuste de cuentas",
         descripcion: "Temporada 2, Episodio 7",
         url: "https://watch.thechosen.tv/video/184683594359?language=en&position=0"
     },
     {
-        titulo: "Episodio 16",
+        titulo: "Más allá de las montañas",
         descripcion: "Temporada 2, Episodio 8",
+        url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
+    },
+    {
+        titulo: "Regreso a casa",
+        descripcion: "Temporada 3, Episodio 1",
+        url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
+    },
+    {
+        titulo: "De dos en dos",
+        descripcion: "Temporada 3, Episodio 2",
+        url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
+    },
+    {
+        titulo: "Médico, cuídate a tí mismo",
+        descripcion: "Temporada 3, Episodio 3",
+        url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
+    },
+    {
+        titulo: "Limpiar (parte 1)",
+        descripcion: "Temporada 3, Episodio 4",
+        url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
+    },
+    {
+        titulo: "Limpiar (parte 2)",
+        descripcion: "Temporada 3, Episodio 5",
+        url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
+    },
+    {
+        titulo: "Intensidad en Tent city",
+        descripcion: "Temporada 3, Episodio 6",
+        url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
+    },
+    {
+        titulo: "Oídos para oir",
+        descripcion: "Temporada 3, Episodio 7",
+        url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
+    },
+    {
+        titulo: "Sustento",
+        descripcion: "Temporada 3, Episodio 8",
+        url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
+    },
+    {
+        titulo: "Promesas",
+        descripcion: "Temporada 4, Episodio 1",
+        url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
+    },
+    {
+        titulo: "Confesiones",
+        descripcion: "Temporada 4, Episodio 2",
+        url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
+    },
+    {
+        titulo: "Luna en sangre",
+        descripcion: "Temporada 4, Episodio 3",
+        url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
+    },
+    {
+        titulo: "Calma previa",
+        descripcion: "Temporada 4, Episodio 4",
+        url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
+    },
+    {
+        titulo: "Esperar, servir y maquinar",
+        descripcion: "Temporada 4, Episodio 5",
+        url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
+    },
+    {
+        titulo: "Dedicación",
+        descripcion: "Temporada 4, Episodio 6",
+        url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
+    },
+    {
+        titulo: "La última señal",
+        descripcion: "Temporada 4, Episodio 7",
+        url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
+    },
+    {
+        titulo: "Humilde",
+        descripcion: "Temporada 4, Episodio 8",
+        url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
+    },
+    {
+        titulo: "Entrada",
+        descripcion: "Temporada 5, Episodio 1",
+        url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
+    },
+    {
+        titulo: "Casa de cartas",
+        descripcion: "Temporada 5, Episodio 2",
+        url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
+    },
+    {
+        titulo: "Lamentos",
+        descripcion: "Temporada 5, Episodio 3",
+        url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
+    },
+    {
+        titulo: "La misma moneda",
+        descripcion: "Temporada 5, Episodio 4",
+        url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
+    },
+    {
+        titulo: "Por mí",
+        descripcion: "Temporada 5, Episodio 5",
+        url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
+    },
+    {
+        titulo: "Reuniones",
+        descripcion: "Temporada 5, Episodio 6",
+        url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
+    },
+    {
+        titulo: "La sala superior (parte 1)",
+        descripcion: "Temporada 5, Episodio 7",
+        url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
+    },
+    {
+        titulo: "La sala superior (parte 2)",
+        descripcion: "Temporada 5, Episodio 8",
         url: "https://watch.thechosen.tv/video/184683594360?language=en&position=0"
     }
 ];
